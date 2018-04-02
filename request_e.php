fe64de79a31d9c8842e0c4212184d599897e991c
Yukiko Hamabe <yukikohama@gmail.com>
2018-04-02 21:41:59 +0900

    Initial commit

---
+++ request_e.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+      <title>Bonjour!</title>
+</head>
+<body>
+Hello <?php print htmlspecialchars($_POST{'name'}, ENT_QUOTES, 'UTF-8'); ?>!
+<br>
+<br>
+<span><img src="../Mini/images/greet.gif" wigth="50" height="80"></span>
+<br>
+<br>
+<a href="index.html" style="text-decoration: none">Back</html>
+</body>
+</html>
---
+++ request_p.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+      <title>Bonjour!</title>
+</head>
+<body>
+Witaj <?php print htmlspecialchars($_POST{'name'}, ENT_QUOTES, 'UTF-8'); ?>!
+<br/>
+<br/>
+<span><img src="../Mini/images/greet.gif" wigth="50" height="80"></span>
+<br>
+<br>
+<br>
+<a href="index.html" style="text-decoration: none">Back</a>
+</body>
+</html>
